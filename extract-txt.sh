479bdbb5a4ca881e2a4f19de062d5d69db120518
Vedant Modi <vedantmodi@gmail.com>
2024-12-10 19:04:28 -0500

    Add install instructions to README

--- extract-txt.sh
+++ extract-txt.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-INSTALL_PATH="/usr/bin/extract-txt"
-sudo cp extract-txt.sh $INSTALL_PATH
-sudo chmod +x $INSTALL_PATH
+FNAME=$1
 
-echo "extract-txt installed successfully at $INSTALL_PATH"
